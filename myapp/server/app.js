228db05d96df165d5f7f6cfcb32b4df9681db59f
ssun <ssun@Lab205bdeiMac-2.local>
2016-04-12 16:11:27 +0800

    setting app.js

--- myapp/server/app.js
+++ myapp/server/app.js
@@ -1,29 +1,54 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var async = require('async');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/routes');
+//var users = require('./routes/users');
 
 var app = express();
-
+// port name ]]]
+app.set('port',3000);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(favicon(__dirname + 'public/favicon.ico'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+
+var server = http.createServer(app);
+server.listen(app.get('port'));
+server.on('listening',function(){
+  console.log('--- listening on port:' + app.get('port') + '---');
+});
+
+server.on('error',function(error){
+  switch (error.code){
+    case 'EACCES' :
+      console.error(bind + '需要权限许可');
+      process.exit(1);
+      break;
+    case 'EACCES' :
+      console.error(bind + '端口已被占用');
+      process.exit(1);
+      break;
+    default :
+      throw error;
+  }
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
