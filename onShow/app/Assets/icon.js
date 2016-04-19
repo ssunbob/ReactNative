25ecb8c59ef0629cc27c5e2b76886a7e77abf328
SSUN <SSUN>
2016-04-19 23:28:43 +0800

    onshow update

---
+++ onShow/app/Assets/icon.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let icons = {
+	display :'',
+	photo :'',
+	about_us :'',
+	artdev :'',
+	about_team :'',
+    store :''
+};
+
+export { icons as default };
--- onShow/index.android.js
+++ onShow/index.android.js
@@ -7,8 +7,9 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  View,
   Text,
-  View
+  PixelRatio//屏幕比特率
 } from 'react-native';
 
 class onShow extends Component {
@@ -16,19 +17,33 @@ class onShow extends Component {
     return (
       <View style={styles.grid}>
 				<View style={styles.grid_row}>
-					<Text style={styles.grid_item}>1</Text>
-					<Text style={styles.grid_item}>2</Text>
+					<View style={styles.grid_item}>
+             <Text style={styles.grid_text}>轮播图</Text>
+          </View>
 				</View>
 				<View style={styles.grid_row}>
-					<Text style={styles.grid_item}>1</Text>
-					<Text style={styles.grid_item}>2</Text>
-					<Text style={styles.grid_item}>3</Text>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Text style={styles.grid_text}>作品</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+             <Text style={styles.grid_text}>开发博客</Text>
+          </View>
 				</View>
 				<View style={styles.grid_row}>
-					<Text style={styles.grid_item}>1</Text>
-					<Text style={styles.grid_item}>2</Text>
-					<Text style={styles.grid_item}>3</Text>
-					<Text style={styles.grid_item}>4</Text>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Text style={styles.grid_text}>图片站</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+             <Text style={styles.grid_text}>团队介绍</Text>
+          </View>
+				</View>
+				<View style={styles.grid_row}>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Text style={styles.grid_text}>关于我们</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+             <Text style={styles.grid_text}>商城</Text>
+          </View>
 				</View>
       </View>
     );
@@ -37,21 +52,33 @@ class onShow extends Component {
 
 const styles = StyleSheet.create({
 	grid:{
-		padding:5,
+		padding:10/PixelRatio.get(),
 		marginTop:25,
 		borderColor:'#e7e7e7',
 	},
 	grid_row:{
 		flex:1,
 		flexDirection:'row',
-		padding:5,
+		padding:10/PixelRatio.get(),
 	},
 	grid_item:{
 		flex:1,
 		backgroundColor:'#e7e7e7',
-		height:30,
-		textAlign:'center',
+		//height:270/PixelRatio.get(),
+    
+    padding:20/PixelRatio.get(),
+    justifyContent:'center',
+  	alignItems:'center',
 	},
+  grid_text:{
+ 		textAlign:'center',   
+  },
+  grid_left:{
+  marginRight:10/PixelRatio.get(),
+  },
+  grid_right:{
+  marginLeft:10/PixelRatio.get(),
+  },
 });
 
 AppRegistry.registerComponent('onShow', () => onShow);
