7fba56506700279880ac2e8597b38f680c791871
SSUN <SSUN>
2016-04-20 13:29:57 +0800

    onshow update

--- onShow/index.android.js
+++ onShow/index.android.js
@@ -8,44 +8,56 @@ import React, {
   Component,
   StyleSheet,
   View,
+  ScrollView,
   Text,
-  PixelRatio//屏幕比特率
+  Image,
+  PixelRatio,//屏幕比特率
+  Dimensions,
 } from 'react-native';
 
 class onShow extends Component {
   render() {
     return (
+      <ScrollView>
       <View style={styles.grid}>
 				<View style={styles.grid_row}>
-					<View style={styles.grid_item}>
-             <Text style={styles.grid_text}>轮播图</Text>
+					<View style={[styles.grid_item,styles.grid_crousel]}>
+            <Image style={styles.icon} source={require('./app/Assets/images.png')} resizeMode={Image.resizeMode.contain}/>
+            <Text style={styles.grid_text}>轮播图</Text>
           </View>
 				</View>
 				<View style={styles.grid_row}>
 					<View style={[styles.grid_item,styles.grid_left]}>
-             <Text style={styles.grid_text}>作品</Text>
+             <Image style={styles.icon} source={require('./app/Assets/map.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>作品展示</Text>
           </View>
 					<View style={[styles.grid_item,styles.grid_right]}>
+             <Image style={styles.icon} source={require('./app/Assets/quill.png')} resizeMode={Image.resizeMode.contain}/>
              <Text style={styles.grid_text}>开发博客</Text>
           </View>
 				</View>
 				<View style={styles.grid_row}>
 					<View style={[styles.grid_item,styles.grid_left]}>
+             <Image style={styles.icon} source={require('./app/Assets/images.png')} resizeMode={Image.resizeMode.contain}/>
              <Text style={styles.grid_text}>图片站</Text>
           </View>
 					<View style={[styles.grid_item,styles.grid_right]}>
+             <Image style={styles.icon} source={require('./app/Assets/magic-wand.png')} resizeMode={Image.resizeMode.contain}/>
              <Text style={styles.grid_text}>团队介绍</Text>
           </View>
 				</View>
 				<View style={styles.grid_row}>
 					<View style={[styles.grid_item,styles.grid_left]}>
+             <Image style={styles.icon} source={require('./app/Assets/users.png')} resizeMode={Image.resizeMode.contain}/>
              <Text style={styles.grid_text}>关于我们</Text>
           </View>
 					<View style={[styles.grid_item,styles.grid_right]}>
+              <Image style={styles.icon} source={require('./app/Assets/cart.png')} resizeMode={Image.resizeMode.contain}/>
              <Text style={styles.grid_text}>商城</Text>
           </View>
 				</View>
       </View>
+     </ScrollView>
     );
   }
 }
@@ -64,14 +76,16 @@ const styles = StyleSheet.create({
 	grid_item:{
 		flex:1,
 		backgroundColor:'#e7e7e7',
-		//height:270/PixelRatio.get(),
-    
+		height:200/PixelRatio.get(),
     padding:20/PixelRatio.get(),
     justifyContent:'center',
   	alignItems:'center',
 	},
+  grid_crousel:{
+    height:270/PixelRatio.get(),
+  },
   grid_text:{
- 		textAlign:'center',   
+ 	  textAlign:'center',   
   },
   grid_left:{
   marginRight:10/PixelRatio.get(),
@@ -79,6 +93,10 @@ const styles = StyleSheet.create({
   grid_right:{
   marginLeft:10/PixelRatio.get(),
   },
+  icon:{
+  width: 32,
+  height: 32
+  }
 });
 
 AppRegistry.registerComponent('onShow', () => onShow);
--- onShow/index.ios.js
+++ onShow/index.ios.js
@@ -1,53 +1,102 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  View,
+  ScrollView,
   Text,
-  View
+  Image,
+  PixelRatio,//屏幕比特率
 } from 'react-native';
 
 class onShow extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <ScrollView>
+      <View style={styles.grid}>
+				<View style={styles.grid_row}>
+					<View style={[styles.grid_item,styles.grid_crousel]}>
+            <Image style={styles.icon} source={require('./app/Assets/images.png')} resizeMode={Image.resizeMode.contain}/>
+            <Text style={styles.grid_text}>轮播图</Text>
+          </View>
+				</View>
+				<View style={styles.grid_row}>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Image style={styles.icon} source={require('./app/Assets/map.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>作品展示</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+             <Image style={styles.icon} source={require('./app/Assets/quill.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>开发博客</Text>
+          </View>
+				</View>
+				<View style={styles.grid_row}>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Image style={styles.icon} source={require('./app/Assets/images.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>图片站</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+             <Image style={styles.icon} source={require('./app/Assets/magic-wand.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>团队介绍</Text>
+          </View>
+				</View>
+				<View style={styles.grid_row}>
+					<View style={[styles.grid_item,styles.grid_left]}>
+             <Image style={styles.icon} source={require('./app/Assets/users.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>关于我们</Text>
+          </View>
+					<View style={[styles.grid_item,styles.grid_right]}>
+              <Image style={styles.icon} source={require('./app/Assets/cart.png')} resizeMode={Image.resizeMode.contain}/>
+             <Text style={styles.grid_text}>商城</Text>
+          </View>
+				</View>
       </View>
+     </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+	grid:{
+		padding:10/PixelRatio.get(),
+		marginTop:25,
+		borderColor:'#e7e7e7',
+	},
+	grid_row:{
+		flex:1,
+		flexDirection:'row',
+		padding:10/PixelRatio.get(),
+	},
+	grid_item:{
+		flex:1,
+		backgroundColor:'#e7e7e7',
+		height:200/PixelRatio.get(),
+    padding:20/PixelRatio.get(),
+    justifyContent:'center',
+  	alignItems:'center',
+	},
+  grid_crousel:{
+    height:270/PixelRatio.get(),
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  grid_text:{
+    paddingTop:20/PixelRatio.get(),
+    textAlign:'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  grid_left:{
+  marginRight:10/PixelRatio.get(),
   },
+  grid_right:{
+  marginLeft:10/PixelRatio.get(),
+  },
+  icon:{
+  width: 32,
+  height: 32
+  }
 });
 
 AppRegistry.registerComponent('onShow', () => onShow);
